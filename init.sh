104ce84e435a0ab945d47d3f74d112a67adc9b3a
Eugene <golubcov.e@gmail.com>
2016-03-17 14:16:01 +0300

    auth

---
+++ init.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+sudo rm /etc/nginx/sites-enabled/default
+
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+
+sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -s /home/box/web/etc/gunicorn_ask.conf /etc/gunicorn.d/ask
+
+
+sudo /etc/init.d/mysql restart
+#MySQL initial actions
+mysql -uroot -e"CREATE DATABASE stepic"
+mysql -uroot -e"CREATE USER 'admin'@'localhost' IDENTIFIED BY 'qwerty'"
+mysql -uroot -e"GRANT ALL ON stepic.* TO 'admin'@'localhost'"
+
+sudo python /home/box/web/ask/manage.py syncdb
+
+sudo /etc/init.d/gunicorn restart
